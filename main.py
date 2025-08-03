9f44d5de32523b10b353aa92d4f29f9b51c3813c
reallemon <spencerpenrod@gmail.com>
2025-08-03 10:36:39 -0600

    feat(main)!: add word count function
    Print the word count instead of the entire book

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ def get_book_text(filepath):
   with open(filepath) as f:
     return f.read()
 
+def word_count(book):
+  words = book.split()
+  return len(words)
+
 def main():
   book_text = get_book_text("books/frankenstein.txt")
-  print(book_text)
+  print(f"{word_count(book_text)} words found in the document")
 
 main()
