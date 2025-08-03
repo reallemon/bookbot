6ff7d2591e4f44aa9cd0bd0a680e08a09cfba1ae
reallemon <spencerpenrod@gmail.com>
2025-08-03 10:39:42 -0600

    refactor(stats): create stats file
    Move stats functions to their own file

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
+from stats import word_count
+
 def get_book_text(filepath):
   with open(filepath) as f:
     return f.read()
-
-def word_count(book):
-  words = book.split()
-  return len(words)
 
 def main():
   book_text = get_book_text("books/frankenstein.txt")
