dcd007a43e104bab08140745aa0bfb5c9cd7a079
reallemon <spencerpenrod@gmail.com>
2025-08-03 10:53:26 -0600

    feat(stats): add character counting

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import word_count
+from stats import word_count, character_count
 
 def get_book_text(filepath):
   with open(filepath) as f:
@@ -7,5 +7,6 @@ def get_book_text(filepath):
 def main():
   book_text = get_book_text("books/frankenstein.txt")
   print(f"{word_count(book_text)} words found in the document")
+  print(character_count(book_text))
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,14 @@
 def word_count(book):
   words = book.split()
   return len(words)
+
+def character_count(book):
+  character_counts = {}
+  characters = list(book)
+  for character in characters:
+    lower_character = character.lower()
+    if (lower_character in character_counts):
+      character_counts[lower_character] += 1
+    else:
+      character_counts[lower_character] = 1
+  return character_counts
